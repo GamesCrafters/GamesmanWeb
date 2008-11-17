57196ce4205716f8a3e0b436b59981c5c304a95d
pathorn <pathorn@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2008-11-17 22:10:04 +0000

    Removed jgamesman directory (now in branches/GamesmanWeb-JNI) to be replaced by a simple tcp client.
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanWeb@414 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- src/edu/berkeley/jgamesman/rebuild-gamesman.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pushd ../../../../../Gamesman+-/ 
-find . \( -name '*.o' -o -name '*.a' \) -exec rm -v {} \;
-make -j4 all
-popd
-
--- src/edu/berkeley/jgamesman/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#-Djava.library.path=$PWD 
-
-cd ../../..
-# use -verbose:class -verbose:jni
-
-java edu.berkeley.jgamesman.Gamesman "$1"
-
