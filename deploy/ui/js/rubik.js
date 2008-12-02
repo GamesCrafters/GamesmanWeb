9913235f8da7d7141ec7e049ea53e176333d75ab
james_ide <james_ide@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2008-12-02 06:06:35 +0000

    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanWeb@510 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- deploy/ui/js/rubik.js
+++ deploy/ui/js/rubik.js
@@ -72,7 +72,7 @@ function updateMoveValues(nextMoves){
     // reset everything first
     clearMoveValues();
 
-	document.getElementById('optimalMove').style.display = 'block';
+	$('#optimalMove').show();
     msg = ''
     for(i in nextMoves) {
     	msg += " | " + nextMoves[i].remoteness + ": " + nextMoves[i].move;
@@ -85,12 +85,12 @@ function updateMoveValues(nextMoves){
 
 // remove all indicators of move values
 function clearMoveValues(){
-	document.getElementById('optimalMove').style.display = 'none';
+	$('#optimalMove').hide();
 }
 
 // converts our own representation of the board (2d/3d array) into a board string
 function getBoardString(board){
-	return document.getElementById('cube').getBoardString();
+	return $("#cube").get(0).getBoardString();
 }
 
 // local debugging
