f94c2b3204f867f8fc5043a9c2b62a2d3e9f373a
ziangxie <ziangxie@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2010-05-18 01:27:05 +0000

    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanWeb@5110 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- deploy/ui/game/js/connections.js
+++ deploy/ui/game/js/connections.js
@@ -55,7 +55,6 @@ Connections.prototype.createParameterString = function() {
 Connections.prototype.start = function(team) {
   var TURN = 0;
   this.player = team || GCWeb.Team.BLUE;
-  this.switchTeams();
   Connections.superClass.start.call(this);
 }
 
