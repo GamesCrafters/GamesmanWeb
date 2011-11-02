3308b5341d5bc9fc30a0f227b682332aa4cbadaf
borislavpetrov <borislavpetrov@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2011-11-02 08:10:25 +0000

    fixed canvas width again, this time with jQuery
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanWeb@6381 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- deploy/ui/game/js/quarto.js
+++ deploy/ui/game/js/quarto.js
@@ -5,8 +5,8 @@ function initQuarto() {
     Quarto.IMAGE_DIR = "game/images/quarto/";
     Quarto.canvas = document.getElementById('canvas');
     Quarto.ctx = Quarto.canvas.getContext('2d');
-    Quarto.canvas.width = Math.min(window.innerHeight-Quarto.canvas.offsetTop, window.innerWidth-Quarto.canvas.offsetTop)*0.92;
-    Quarto.canvas.height = Math.min(window.innerHeight-Quarto.canvas.offsetTop, window.innerWidth-Quarto.canvas.offsetTop)*0.92;
+    Quarto.canvas.width = Math.min($('#canvasContainer').innerHeight()-Quarto.canvas.offsetTop, $('#canvasContainer').innerWidth()-Quarto.canvas.offsetTop)*0.92;
+    Quarto.canvas.height = Math.min($('#canvasContainer').innerHeight()-Quarto.canvas.offsetTop, $('#canvasContainer').innerWidth()-Quarto.canvas.offsetTop)*0.92;
 
     Quarto.TOTAL_WIDTH = Quarto.canvas.width;
     Quarto.TOTAL_HEIGHT = Quarto.canvas.height;
