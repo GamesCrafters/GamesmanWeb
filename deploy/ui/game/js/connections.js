57366c254a80eb2e923cfcb12e9cf50d522e4c53
borislavpetrov <borislavpetrov@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2011-11-23 09:36:21 +0000

    jack's latest additions to connections
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanWeb@6427 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- deploy/ui/game/js/connections.js
+++ deploy/ui/game/js/connections.js
@@ -6,10 +6,10 @@ var boardSize; //Size of board
 var divFactor; //Number of cols of the grid: size 2 = 5x5 grid = divFactor of 5
 var componentLength; //Length of one unit on the grid
 var boardString; //String that represents the board
+var player = 1;
 $(document).ready(
 function() 
 	{
-		$('#canvasContainer').append('<canvas id="canvas"></canvas>');
 		canvas = document.getElementById('canvas');
 		canvas.width = (window.innerHeight-canvas.offsetTop)*0.92;
         canvas.height = (window.innerHeight-canvas.offsetTop)*0.92;
@@ -20,12 +20,12 @@ function()
 		boardSize = 3;
 		//boardString = "20000001020002000301000000"
 		//boardString = "20000001020002000301000000"
-		boardString = "30000000000001000000000002000000010000000000000000"
-		drawBoard(boardSize, boardString);
+		boardString = "30000000000000000000000000000000000000000000000000"
+		drawBoard(boardSize, boardString, player);
 	}	
 )
 
-function drawBoard(size, board){ //size is an int, board is a string depiction
+function drawBoard(size, board, player){ //size is an int, board is a string depiction, player is player #
 	ctx.fillStyle = "#E8E8E8"
 	ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 	divFactor = size*2+1;
@@ -47,33 +47,45 @@ function drawBoard(size, board){ //size is an int, board is a string depiction
 	//Draws in board
 	var boardArray = makeArray(board);
 	drawArray(boardArray);
+	drawNextMoves(player, boardArray);
 }
 	
 function drawSquares(){
 	//draws player one's squares
-	ctx.fillStyle = "blue";
 	for (a = 1; a < divFactor; a+=2){
 		for (b = 0; b <= divFactor; b+=2){
-			drawOneSquare(a*componentLength, b*componentLength);
+			drawOneSquare(a, b, 0.6, "blue");
 		}
 	}
-	ctx.fillStyle = "red";
 	for (a = 0; a <= divFactor; a+=2){
 		for (b = 1; b < divFactor; b+=2){
-			drawOneSquare(a*componentLength, b*componentLength);
+			drawOneSquare(a, b, 0.6, "red");
 		}
 	}
 }
-function drawOneSquare(x, y){
-	var squareOffset = componentLength * 0.2
-	ctx.fillRect(x+squareOffset, y+squareOffset,
+function drawOneSquare(x, y, scale, color){ //Fills in a square at x, y
+	var offset = (1-scale) / 2
+	ctx.fillStyle = color;	
+	var squareOffset = componentLength * offset;
+	ctx.fillRect(x*componentLength+squareOffset, y*componentLength+squareOffset,
 		componentLength-2*squareOffset, componentLength-2*squareOffset);
 }
 
 function drawArray(array){
 	for(x = 0; x < array.length; x++){
-		for(y = 0; y < array.length; y++)
+		for(y = 0; y < array.length; y++){
 			drawLine(x, y, array[x][y]);
+		}
+	}
+}
+
+function drawNextMoves(player, array){ //Fills in possible next moves
+	for(x = 1; x < divFactor - 1; x++){
+		for(y = 1; y < divFactor - 1; y++){
+			if (array[x][y] == 0 && (x+y)%2 == 0){
+				drawOneSquare(x, y, 1, "33CCCC");
+			}
+		}
 	}
 }
 
@@ -150,19 +162,35 @@ function connectionsClick(e) {
 	var y = e.clientY-canvas.offsetTop + document.body.scrollTop + document.documentElement.scrollTop;
 	console.log("x, y: ", x, y);
 	//Now we need to convert x and y into board coordinates
-	x = parseInt(x / canvasWidth * divFactor);
-	y = parseInt(y / canvasHeight * divFactor);
+	if (x >= 0){
+		x = parseInt(x / canvasWidth * divFactor);
+		}
+	else{x = -1;}
+	if (y >= 0){
+		y = parseInt(y / canvasHeight * divFactor);
+		}
+	else{y = -1;}
 	console.log("Converted x, y: ", x, y);
-	boardString = changeBoard(boardString, x, y);
-	console.log("New boardString: ", boardString);
-	drawBoard(boardSize, boardString);
+	if (x >= 0 && y >= 0 && x < divFactor && y < divFactor) {
+		boardString = changeBoard(boardString, x, y, player);
+		console.log("New boardString: ", boardString);
+		drawBoard(boardSize, boardString);
+		player = player%2 + 1;
+		console.log("Next Player: ", player);
+		drawNextMoves(player, boardString);
+		}
 }
 
-function changeBoard(board, x, y) {
+function changeBoard(board, x, y, player) {
 	var newBoard = board;
 	//Change the string board at coordinate x, y
 	var strPos = y*divFactor + x + 1
-	newBoard = newBoard.substr(0, strPos) + (newBoard.charAt(strPos)+1)%3 + newBoard.substr(strPos + 1)
+	if (board.charAt(strPos) == 0) {
+		newBoard = newBoard.substr(0, strPos) + player + newBoard.substr(strPos + 1);
+	}
+	else {
+		newBoard = newBoard.substr(0, strPos) + 0 + newBoard.substr(strPos + 1);
+	}
 	return newBoard;
 }
 
