8c0444cf32ba40c91daceed94fd75c349f7f81c0
zipcodeman <zipcodeman@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2012-02-29 20:27:09 +0000

    Added undo/redo and new flow of object
    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanWeb@6641 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- html5/GCAPI/GCAPI.js
+++ html5/GCAPI/GCAPI.js
@@ -1,105 +1,108 @@
-function GCConnection(name, notifierClass){
+function Game(name, height, width, notifierClass, board){
   this.gameName = name;
-  this.height = 0;
-  this.width = 0;
+  this.height = height;
+  this.width = width;
   this.misere = false;
   this.pieces = 0;
   this.notifier = notifierClass;
+  this.previousBoards = Array();
+  this.nextBoards = Array();
+  this.currentBoard = board;
   this.baseUrl = "http://nyc.cs.berkeley.edu:8080/gcweb/service/" + 
                  "gamesman/puzzles/";
-
-  this.setBoardHeight = function(height){
-    this.height = height;
-  }
-
-  this.getBoardHeight = function(){
-    return this.height;
-  }
-
-  this.setBoardWidth = function(width){
-    this.width = width;
-  }
-
-  this.getBoardWidth = function(){
-    return this.width;
-  }
-
-  this.setMisere = function(misere){
-    this.misere = misere;
-  }
-
-  this.getMisere = function(){
-    return this.misere;
-  }
-
-  this.setPieces = function(pieces){
-    this.pieces = pieces;
-  }
-
-  this.setDrawProcedure = function(draw){
-    this.draw = draw;
-  }
-
-  this.getUrlTail = function(board){
-    return ";width=" + this.width + ";height=" + this.height + 
-           ";pieces=" + this.pieces + ";board=" + escape(board);
-  }
-  this.getBoardValues = function(board, notifier){
-    requestUrl = this.baseUrl + this.gameName + "/getMoveValue" +
-                 this.getUrlTail(board)
-    $.ajax({
-      url: requestUrl,
-      dataType: "json",
-      success: function(data){
-        notifier(data)
-      },
-    });
-  }
-
-  this.getPossibleMoves = function(board, notifier){
-    requestUrl = this.baseUrl + this.gameName + "/getNextMoveValues" +
-                 this.getUrlTail(board)
-    $.ajax({
-      url: requestUrl,
-      dataType: "json",
-      success: function(data){
-        notifier(data)
-      }
-    });
-  }
-
-  this.storeBoardValue = function(value){
-    this.value = value
-    this.getPossibleMoves(value['response']['board'], this.storePossibleMoves)
-  }
-
-  this.storePossibleMoves = function(moves){
-    this.moves = moves;
-    retval = Object();
-    retval.value = this.value
-    retval.moves = this.moves
-    this.notifier.draw(retval)
-  }
-
-  this.makeMove = function(board){
-    this.getBoardValues(board, this.storeBoardValue)
-  }
 }
 
-function tttNotify(){
-  this.draw = function(data){
-    alert(JSON.stringify(data))
-  }
+Game.prototype.setBoardHeight = function(height){
+  this.height = height;
+}
+
+Game.prototype.getBoardHeight = function(){
+  return this.height;
+}
+
+Game.prototype.setBoardWidth = function(width){
+  this.width = width;
+}
+
+Game.prototype.getBoardWidth = function(){
+  return this.width;
+}
+
+Game.prototype.setMisere = function(misere){
+  this.misere = misere;
+}
+
+Game.prototype.getMisere = function(){
+  return this.misere;
+}
+
+Game.prototype.setPieces = function(pieces){
+  this.pieces = pieces;
+}
+
+Game.prototype.setDrawProcedure = function(draw){
+  this.draw = draw;
+}
+
+Game.prototype.getUrlTail = function(board){
+  return ";width=" + this.width + ";height=" + this.height + 
+         ";pieces=" + this.pieces + ";board=" + escape(board);
+}
+Game.prototype.getBoardValues = function(board, notifier){
+  requestUrl = this.baseUrl + this.gameName + "/getMoveValue" +
+               this.getUrlTail(board)
+  $.ajax({
+    url: requestUrl,
+    dataType: "json",
+    success: function(data){
+      notifier(data)
+    },
+  });
+}
+
+Game.prototype.getPossibleMoves = function(board, notifier){
+  requestUrl = this.baseUrl + this.gameName + "/getNextMoveValues" +
+               this.getUrlTail(board)
+  $.ajax({
+    url: requestUrl,
+    dataType: "json",
+    success: function(data){
+      retval = Array()
+      if(data.status == "ok"){
+        notifier(data.response)
+      }else{
+        notifier(data)
+      }
+    }
+  });
+}
+
+Game.prototype.undo = function(){
+  // Undo here
+}
+
+Game.prototype.redo = function(){
+  // redo here.
+}
+
+Game.prototype.startGame = function(){
+  this.notifier.drawBoard(this.currentBoard)
+  this.getPossibleMoves(this.currentBoard, this.notifier.drawMoves)
+}
+
+function TTTNotify(canvas){
+  this.canvas = canvas;
+}
+
+TTTNotify.prototype.drawBoard = function(board){
+}
+
+TTTNotify.prototype.drawMoves = function(data){
+  alert(JSON.stringify(data))
 }
 
 function loadBoard(){
-  notifier = new tttNotify()
-  data = new GCConnection("ttt", notifier);
-  data.setBoardHeight(3);
-  data.setBoardWidth(3);
-  data.getPossibleMoves("         ", 
-    function(d){ 
-      data.makeMove(d['response'][0]['board'])
-    }
-  );
+  notifier = new TTTNotify()
+  data = new Game("ttt", 3, 3, notifier, "         ");
+  data.startGame()
 }
