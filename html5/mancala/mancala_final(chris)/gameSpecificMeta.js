7cd7497d1e50b91d2fc156bed49ec1403279eadd
jernchr <jernchr@a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba>
2012-05-01 22:14:53 +0000

    git-svn-id: https://gamescrafters.svn.sourceforge.net/svnroot/gamescrafters/GamesmanWeb@6914 a668be5d-ef11-cb4c-8bfd-e08cbb24f2ba

--- html5/mancala/mancala_final(chris)/gameSpecificMeta.js
+++ html5/mancala/mancala_final(chris)/gameSpecificMeta.js
@@ -1,11 +1,9 @@
-1//with animation
-
 /*Variables from interface:
 var PLAYER1 = true;
 var PLAYER2 = false;
 var playerTurn = PLAYER1;
-var player1Name = "Bob";
-var player2Name = "Annie";
+var player1Name = "Player 1";
+var player2Name = "Player 2";
 var player1AI = false;
 var player2AI = false;
 var randomness1 = 0; // Only applicable if there is AI involved.
@@ -30,6 +28,8 @@ var gameSpecificOption3Name = "Input name here";
 /**
 WRITE YOUR CODE HERE! WRITE YOUR CODE HERE! WRITE YOUR CODE HERE! WRITE YOUR CODE HERE! WRITE YOUR CODE HERE! WRITE YOUR CODE HERE!
 */
+//Variables needed for interaction with server
+var canTalkToServer = false;
 
 //Initialization variables
 var initialized = false;
@@ -38,6 +38,7 @@ var imageLoaded = false;
 //Preference, and static variables
 var PITS_PER_PLAYER = 6;
 var PEBBLES_PER_PIT = 4;
+var DEFAULT_PIT_COLOR = "rgba(0,0,0,0.5)";
 var WIDTH = (1380)/(2+PITS_PER_PLAYER);
 var HEIGHT = WIDTH;
 var verticalMargin = 0;
@@ -46,6 +47,7 @@ var boardX = 0;
 var boardY = 0;
 var boardH = 0;
 var boardW = 0;
+
 //Used for detection of clicks on the scaled board
 var boardImage;
 var bufferContext;
@@ -54,6 +56,8 @@ var bufferCanvas;
 //Game instance variables
 var pits;
 var waitingForInput;
+var animateLock;
+var moveInterval;
 
 /* Functions */
 
@@ -72,16 +76,6 @@ function drawBoardToBuffer(){
 	bufferContext.drawImage(boardImage,0,0);
 	drawPitsToBuffer();
 	drawInfoToBuffer();
-}
-
-function drawPebblesToBuffer() {
-	for (var i = 0; i < (2+ 2*PITS_PER_PLAYER); i++) {
-		if (pits[i].getNumOfPebbles() != 0) {
-			for (var k = 0; k < pits[i].getNumOfPebbles(); k++) {
-				pits[i].pebbles[k].update();
-			}
-		}
-	}
 }
 	
 //Function that takes the buffer and draws it onto the scaled canvas gameInterface
@@ -123,7 +117,7 @@ function drawBufferToCanvas(){
 function drawPitsToBuffer(){
 	bufferContext.save();
 
-	var vertPadding = Math.floor(bufferCanvas.height/20)
+	var vertPadding = Math.floor(bufferCanvas.height/20);
 	var horizPadding = 10;
 	var pitWidth = Math.floor((bufferCanvas.width-10)/(PITS_PER_PLAYER + 2));
 	var pitHeight = 6*vertPadding;
@@ -145,23 +139,33 @@ function drawPitsToBuffer(){
 	//Draw player1 pit
 	bufferContext.fillRect(horizPadding,vertPadding,playerPitWidth,playerPitHeight);
 	bufferContext.fillStyle = "rgba(256,256,256,0.8)";
-	bufferContext.fillText(p1Text,horizPadding+10,vertPadding+(playerPitWidth-40));
+	bufferContext.fillText(p1Text,horizPadding+10,vertPadding+Math.floor(playerPitHeight/2));
 	
 	bufferContext.fillStyle = "rgba(0,0,0,0.5)";
 	//Draw Player2 pit
 	bufferContext.fillRect(bufferCanvas.width-horizPadding-playerPitWidth,vertPadding,playerPitWidth,playerPitHeight);
 	bufferContext.fillStyle = "rgba(256,256,256,0.8)";
-	bufferContext.fillText(p2Text,bufferCanvas.width-horizPadding-playerPitWidth+10,vertPadding+(playerPitWidth-40));
+	bufferContext.fillText(p2Text,bufferCanvas.width-horizPadding-playerPitWidth+10,vertPadding+Math.floor(playerPitHeight/2));
 
 	var startingPoint = horizPadding + playerPitWidth;
 	//Draw the individual pits on top
 	for(var i=0;i<PITS_PER_PLAYER;i++){
+		var pitColor = DEFAULT_PIT_COLOR;
 		var number = pits[getPitIndex(i+1,0)].getNumOfPebbles();
 		var text = "" + number;
 		if(number<10){
 			text = "0" + number;
 		}
-		bufferContext.fillStyle = "rgba(0,0,0,0.5)";
+	
+		//Determine pit color (move values and delta remoteness)
+		if(waitingForInput && (playerTurn == PLAYER2) && showMoveValues){
+			pitColor = pits[getPitIndex(i+1,0)].color;
+			if(!showDeltaRemoteness){
+				pitColor = pitColor.substring(0,pits[getPitIndex(i+1,0)].color.length-4) + "1)";
+			}
+		}
+
+		bufferContext.fillStyle = pitColor;
 		bufferContext.fillRect(startingPoint+horizPadding+i*pitWidth,vertPadding,pitWidth-2*horizPadding,pitHeight-2*vertPadding);
 		bufferContext.fillStyle = "rgba(256,256,256,0.8)";
 		bufferContext.fillText(text,startingPoint+horizPadding+i*pitWidth+10,vertPadding+pitWidth-50);
@@ -169,12 +173,22 @@ function drawPitsToBuffer(){
 
 	//Draw the individual pits on bottom
 	for(var i=0;i<PITS_PER_PLAYER;i++){
+		var pitColor = DEFAULT_PIT_COLOR;
 		var number = pits[i].getNumOfPebbles();
 		var text = "" + number;
 		if(number<10){
 			text = "0" + number;
 		}
-		bufferContext.fillStyle = "rgba(0,0,0,0.5)";
+
+		//Determine pit color (move values and delta remoteness)
+		if(waitingForInput && (playerTurn == PLAYER1) && showMoveValues){
+			pitColor = pits[i].color;
+			if(!showDeltaRemoteness){
+				pitColor = pitColor.substring(0,pits[i].color.length-4) + "1)";
+			}
+		}
+
+		bufferContext.fillStyle = pitColor;
 		bufferContext.fillRect(startingPoint+horizPadding+i*pitWidth,bufferCanvas.height-pitHeight+vertPadding,
 				pitWidth-2*horizPadding,pitHeight-2*vertPadding);
 		bufferContext.fillStyle = "rgba(256,256,256,0.8)";
@@ -185,13 +199,90 @@ function drawPitsToBuffer(){
 	bufferContext.restore();
 }
 
+var indexGlobal = 0;
+var numPebblesGlobal = 0;
+var srcPitIndexGlobal = 0;
+var destPitIndexGlobal = null;
+var srcPitGlobal = null;
+var distance = 0;
+	var goalx = 0;
+	var goaly = 0;
+	var angle = 0;
+	var v = 0.40;
+	var movePebble = null;
+	var animateLock = false;
+function drawPebblesToBuffer() {
+	for (var i = 0; i < (2+ 2*PITS_PER_PLAYER); i++) {
+		if (pits[i].getNumOfPebbles() != 0) {
+			for (var k = 0; k < pits[i].getNumOfPebbles(); k++) {
+				pits[i].pebbles[k].update();
+			}
+		}
+	}
+}
+//returns the rgba color of the pit ***Random for now***
+function getMoveValue(pitIndex){
+	var randomColor = Math.floor(Math.random()*3);
+	var colorToRet = "rgba(";
+	var randomDelta = 0;
+	while(randomDelta < 0.1){
+		randomDelta = Math.floor(10 * Math.random())/10;
+	}
+	
+	if(randomColor == 2){
+		colorToRet += "255,0,0,";
+	}else if(randomColor == 1){
+		colorToRet += "0,255,0,";
+	}else{
+		colorToRet += "255,255,0,";
+	}
+
+	if(randomColor != 0){
+		colorToRet += randomDelta + ")";
+	}else{
+		colorToRet += "1.0)";
+	}
+
+	if(pits[pitIndex].getNumOfPebbles() == 0){
+		colorToRet = "rgba(255,0,0,1.0)";
+	}
+	return colorToRet;
+}
 //This function is used to draw player names to the board and stuff like that
 function drawInfoToBuffer(){
-	//TODO
 	//Use verticalMargin and horizontalMargin
+	var pitWidth = Math.floor((bufferCanvas.width-10)/(PITS_PER_PLAYER + 2));
+
+	//Player 2
+	bufferContext.save();
+	bufferContext.font = Math.floor(pitWidth/3) + "px Arial";
+	if(playerTurn == PLAYER2){
+		bufferContext.shadowColor = 'black';
+		bufferContext.shadowOffsetX = 5;
+		bufferContext.shadowOffsetY = 5;
+		bufferContext.shadowBlur = 20;
+	}
+	bufferContext.fillStyle = "rgba(0,0,0,0.6)";
+	bufferContext.textAlign = "center";
+	bufferContext.fillText(player2Name,700,pitWidth+60);
+	bufferContext.restore();
+
+	//Player 1
+	bufferContext.save();
+	bufferContext.font = Math.floor(pitWidth/3) + "px Arial";
+	if(playerTurn == PLAYER1){
+		bufferContext.shadowColor = 'black';
+		bufferContext.shadowOffsetX = 5;
+		bufferContext.shadowOffsetY = 5;
+		bufferContext.shadowBlur = 20;
+	}
+	bufferContext.fillStyle = "rgba(0,0,0,0.5)";
+	bufferContext.textAlign = "center";
+	bufferContext.fillText(player1Name,700,700-pitWidth-30);
+	bufferContext.restore();
 }
 
-//Handles click operations. Checks what was chosen, if valid, acts accordingly **DOES NOT WORK FOR MORE THAN 6 PITS YET***
+//Handles click operations. Checks what was chosen, if valid, acts accordingly
 function clickOp(x,y){
 	if(x > boardX && x < (boardW + boardX) && y > boardY && y < (boardH + boardY)){
 		var intervalH = Math.floor(boardW/(2+PITS_PER_PLAYER));
@@ -202,14 +293,16 @@ function clickOp(x,y){
 		var yPos = Math.floor(relativeY/intervalV);
 		
 		//alert("You chose (" + xPos + "," + yPos + ")");
-		if(xPos != 0 && xPos != 7 && yPos == 1 && playerTurn == PLAYER1 && (xPos != 0 && xPos != (PITS_PER_PLAYER+1))){
+		if(xPos != 0 && xPos != (PITS_PER_PLAYER + 1) && yPos == 1 && playerTurn == PLAYER1 && (xPos != 0 && xPos != (PITS_PER_PLAYER+1))){
 			waitingForInput = false;
 			distributePebbles(xPos,yPos);
 			moveLoop();
-		}else if(xPos != 0 && xPos != 7 &&yPos == 0 && playerTurn == PLAYER2 && (xPos != 0 && xPos != (PITS_PER_PLAYER+1))){
+			drawBoard();
+		}else if(xPos != 0 && xPos != (PITS_PER_PLAYER + 1) &&yPos == 0 && playerTurn == PLAYER2 && (xPos != 0 && xPos != (PITS_PER_PLAYER+1))){
 			waitingForInput = false;
 			distributePebbles(xPos,yPos);
 			moveLoop();
+			drawBoard();
 		}else{
 			if(xPos != 0 && xPos != (PITS_PER_PLAYER+1)){
 				if(playerTurn == PLAYER1){
@@ -233,10 +326,16 @@ function getPitIndex(x,y){
 	}
 }
 
-//Takes index of pit from which to take pebbles, and distributes pebbles to appropriate pits **MAKE SURE TO HANDLE ANIMATION**
+//Takes coordinates of pit from which to take pebbles, and distributes pebbles to appropriate pits **MAKE SURE TO HANDLE ANIMATION**
+//If only one variable is input, then it is treated as the index
 function distributePebbles(x,y){
 	//alert("distribute from " + x + "," + y);
-	var srcPitIndex = getPitIndex(x,y);
+	var srcPitIndex;
+	if(y == -1){
+		srcPitIndex = x;
+	}else{
+		srcPitIndex = getPitIndex(x,y);
+	}
 	var srcPit = pits[srcPitIndex];
 	var lastDestIndex = (srcPitIndex+srcPit.getNumOfPebbles())%(2*PITS_PER_PLAYER+2);
 	var numOfPebbles = srcPit.getNumOfPebbles();
@@ -251,37 +350,18 @@ function distributePebbles(x,y){
 	srcPitGlobal = srcPit;
 	distrPebbles();
 	/*
-	for(var i = 0; i < numOfPebbles; i++){
+	for(var i = 0; i < numOfPebbles;i++){
 		var pebble = srcPit.removePebble();
 		var destPitIndex = (srcPitIndex+i+1)%(2*PITS_PER_PLAYER+2);
-		var pebbleDestX = pitIndexToCoor(destPitIndex).x + getRandomComponent() + (WIDTH/2);
-		var pebbleDestY = pitIndexToCoor(destPitIndex).y + getRandomComponent() + (HEIGHT/2);
-		var currentX = pebble.x;
-		var currentY = pebble.y;
-
-		
-		pebble.x =  pebbleDestX;
-		pebble.y =  pebbleDestY;		
-		animateTransition(pebble,srcPitIndex,destPitIndex);
+		//animateTransition(pebble,srcPitIndex,destPitIndex);
 		pits[destPitIndex].addPebble(pebble);
-		//drawBoard();
 	}
-	
-	
 	
 	handleLastLanding(srcPitIndex,lastDestIndex);
 	//Handle change of turn later
-	drawBoard();
 	*/
+	drawBoard();
 }
-
-
-var indexGlobal = 0;
-var numPebblesGlobal = 0;
-var srcPitIndexGlobal = 0;
-var destPitIndexGlobal = null;
-var srcPitGlobal = null;
-
 function distrPebbles() {
 	if (numPebblesGlobal != indexGlobal) {
 		var pebble = srcPitGlobal.removePebble();
@@ -299,10 +379,11 @@ function distrPebbles() {
 		drawBoard();
 		indexGlobal++;
 		animateTransition();
-		setTimeout("distrPebbles()",700+distance);
+		setTimeout("distrPebbles()",500+distance);
 		
 	}
 	else {
+		//alert("runs");
 		var lastDestIndex = (srcPitIndexGlobal+srcPitGlobal.getNumOfPebbles())%(2*PITS_PER_PLAYER+2);
 		handleLastLanding(srcPitIndexGlobal,lastDestIndex);
 		drawBoard();
@@ -310,33 +391,6 @@ function distrPebbles() {
 	}
 	
 	
-}
-	var distance = 0;
-	var goalx = 0;
-	var goaly = 0;
-	var angle = 0;
-	var v = .20;
-	var movePebble = null;
-	var animateLock = false;
-	
-	
-	//Animates the transition of a pebbles between two pits
-function animateTransition(){
-		animateLock = true;
-		distance = Math.sqrt((movePebble.x-goalx)*(movePebble.x-goalx) + (movePebble.y-goaly)*(movePebble.y-goaly));
-		if (distance > 15) {
-			movePebble.x = movePebble.x + distance*v*Math.cos(angle);
-			movePebble.y = movePebble.y + distance*v*Math.sin(angle);
-			drawBoard();
-			setTimeout("animateTransition()", 100);
-		}
-		else {
-			animateLock = false;
-		}
-
-			drawBoardToBuffer();
-			drawPebblesToBuffer();
-			drawBufferToCanvas();
 }
 
 //This function that handles special events where the last pit is placed in a particular place ***NOT DONE***
@@ -352,9 +406,36 @@ function handleLastLanding(srcPitIndex,lastDestIndex){
 //Function that gets move from AI if AI turn, else waits for click from user
 function moveLoop(){
 	if(isAITurn()){
-		distributePebbles(askForMove());
+		clearMoveValues();
+		distributePebbles(askForMove(),-1);
+		setTimeout(moveLoop,0);
 	}else{
+		setMoveValues();
 		waitingForInput = true;
+		drawBoard();
+	}
+}
+
+function pause(msecs){
+	var start = new Date().getTime();
+	var cur = start;
+
+	while(cur - start < msecs){
+		cur = new Date().getTime();
+	}	
+} 
+
+//Sets all of the pit colors to default
+function clearMoveValues(){
+	for(var i = 0; i < pits.lenght; i++){
+		pits[i].setColor(DEFAULT_PIT_COLOR);
+	}
+}
+
+//Sets all of the pit colors to their respective move values
+function setMoveValues(){
+	for(var i = 0; i < pits.length; i++){
+		pits[i].setColor(getMoveValue(i));
 	}
 }
 
@@ -367,9 +448,19 @@ function isAITurn(){
 	}
 }
 
-//This function will ask the mancala AI on server(or code) for a move
+//This function will ask the mancala AI on server(or code) for a move **random for now**
 function askForMove(){
 	//should return an index of the chosen pit
+	var randomNum = PITS_PER_PLAYER;
+	while(randomNum  == PITS_PER_PLAYER){
+		randomNum = Math.floor(PITS_PER_PLAYER * Math.random());
+	}
+
+	if(playerTurn == PLAYER1){
+		return randomNum;
+	}else{
+		return randomNum + PITS_PER_PLAYER + 1;
+	}
 }
 
 //Returns pebbles with random values, which may include pebble color
@@ -380,7 +471,6 @@ function getRandomPebble(x, y){
 	var HEIGHT = WIDTH;
 	return new pebble(x + getRandomComponent(.5) + (WIDTH/2) , y + getRandomComponent(.3) +(HEIGHT/2), 30, 30, getRandomColor() );
 }
-
 function getRandomColor() {
 	var colors = new Array( "red", "blue", "silver", "yellow", "green");
 	return colors[Math.floor(Math.random()*5)];
@@ -392,14 +482,25 @@ function getRandomComponent(scale) {
 	return (Math.floor(Math.random()*radius*2) - radius)*scale;
 }
 
+	//Animates the transition of a pebbles between two pits
+function animateTransition(){
+		animateLock = true;
+		distance = Math.sqrt((movePebble.x-goalx)*(movePebble.x-goalx) + (movePebble.y-goaly)*(movePebble.y-goaly));
+		if (distance > 15) {
+			movePebble.x = movePebble.x + distance*v*Math.cos(angle);
+			movePebble.y = movePebble.y + distance*v*Math.sin(angle);
+			drawBoard();
+			setTimeout("animateTransition()", 100);
+		}
+		else {
+			//animateLock = false;
+		}
 
-
-
-
-
-
-
-
+			drawBoardToBuffer();
+			drawPebblesToBuffer();
+			drawBufferToCanvas();
+}
+//Returns the coordinatePair object of the position of the pit. Takes index of the pit
 function pitIndexToCoor(index){
 	//Calculations from drawPitsToBuffer(). This will be generalized
 	var vertPadding = Math.floor(bufferCanvas.height/20);
@@ -443,43 +544,45 @@ function pitIndexToCoor(index){
 	}
 }
 
-function mancalaDelay(millis) 
-{
-var date = new Date();
-var current = null;	
-do { current = new Date(); } 
-while(current-date < millis);
-} 
-
 /*
 *Objects
 */
-
-function coordinatePair(xCoor,yCoor){
-	this.x = xCoor;
-	this.y = yCoor;
-}
 
 //The pit object
 function pit(){
 	this.pebbles = new Array();
 	this.highlighted = false;
+	this.coordinates = new coordinatePair(0,0);
+	this.color = DEFAULT_PIT_COLOR;
 
-	//Method to add pebble
+	//Add pebble
 	this.addPebble = function(pebble){
 		this.pebbles.push(pebble);
 	}
 	
-	//Method to remove pebble. Return the pebble being removed
+	//Remove pebble. Return the pebble being removed
 	this.removePebble = function(){
 		return this.pebbles.pop();
 	}
 	
-	//Method to get the number of pebbles the pit has
+	//Get the number of pebbles the pit has
 	this.getNumOfPebbles = function(){
 		return this.pebbles.length;
 	}
+	
+	//Get the x and y coordinates, returns a coordinatePair object
+	this.getCoordinates = function(){
+		return this.coordinates;
+	}
+
+	//Sets pit color
+	this.setColor = function(c){
+		this.color = c
+	}
 }
+
+
+
 
 function pebble(x, y, width, height, color) {
 	this.x = x; //x cor
@@ -493,6 +596,11 @@ function pebble(x, y, width, height, color) {
 		bufferContext.drawImage(this.p, this.x - this.width/2, this.y - this.height/2, this.width, this.height)
 	}
 	
+}
+
+function coordinatePair(xCoor,yCoor){
+	this.x = xCoor;
+	this.y = yCoor;
 }
 
 /**
@@ -543,33 +651,33 @@ function upFunction(xPos, yPos) {
 This is the function you will use to initialize your game. It doesn't matter what you do in it, just please keep the position storage in there after you are done initializing. PLEASE Update current position whenever you call this with the boardstate!
 */
 function initializeGame() {
+	pits = new Array();
+	//change to false after modifying functions
+	waitingForInput = false;
+	animateLock = false;
+	
+	//Create and load the board image
+	boardImage = new Image();
+	boardImage.onload = function(){imageLoaded=true;};
+	boardImage.src = "light_wood.png";
+
 	//Create buffer, to not worry about size when drawing to the scaled canvas
 	bufferCanvas = document.createElement("canvas");
 	bufferCanvas.setAttribute("width","1400px");
 	bufferCanvas.setAttribute("height","700px");
 	bufferContext = bufferCanvas.getContext("2d");
-	
-	
-	pits = new Array();
-	//change to false after modifying functions
-	waitingForInput = false;
-	
+
 	//Add the total number of pits, including player main pits, to the pits array
 	for(var i = 0; i < (2 + 2*PITS_PER_PLAYER);i++){
 		pits[i] = new pit();
-
-		//Only add default number of pits if the pit is not either of the players' main pit
+		pits[i].setColor(DEFAULT_PIT_COLOR);
+		//Only add default number of pebble if the pit is not either of the players' main pit
 		if(((i+1)%(PITS_PER_PLAYER+1)) != 0){
 			for(var k = 0; k < PEBBLES_PER_PIT;k++){
 				pits[i].addPebble(getRandomPebble(pitIndexToCoor(i).x, pitIndexToCoor(i).y));
 			}
 		}
 	}
-	
-	//Create and load the board image
-	boardImage = new Image();
-	boardImage.onload = function(){imageLoaded=true;};
-	boardImage.src = "light_wood.png";
 
 	//Set turn to player1
 	playerTurn = PLAYER1;
