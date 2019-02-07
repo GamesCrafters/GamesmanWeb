b07d0fcb2ff61fc4b23f9928edd2c9c8946ca6ff
Zhuo Lu <sethlu@mintkit.net>
2019-02-07 14:35:06 -0800

    Shutdown/startup dev tomcat8 instance correctly

--- launch.sh
+++ launch.sh
@@ -4,24 +4,36 @@
 
 TOMCAT_HOME=/var/lib/tomcat8
 
-if [ $# -gt 0 ]; then # environment provided
-    TOMCAT_HOME="$TOMCAT_HOME-$1"
-    if [ ! -d $TOMCAT_HOME ]; then
-        echo "Environment specified not found: $1"
-        exit
-    fi
-else 
+if [ $# -eq 0 ]; then
+
     read -p "Are you sure? (yes/no) " -n 1 -r
     echo
     if [[ ! $REPLY =~ ^[Yy]$ ]]; then
         exit
     fi
-fi
 
-ant war && (
-systemctl stop tomcat8
-rm -f $TOMCAT_HOME/webapps/gcweb.war
-rm -fR $TOMCAT_HOME/webapps/gcweb
-mv gcweb.war $TOMCAT_HOME/webapps/
-systemctl start tomcat8
-)
+    ant war && (
+        systemctl stop tomcat8
+        rm -f $TOMCAT_HOME/webapps/gcweb.war
+        rm -fR $TOMCAT_HOME/webapps/gcweb
+        mv gcweb.war $TOMCAT_HOME/webapps/
+        systemctl start tomcat8
+    )
+
+else  # Alt environment
+
+    TOMCAT_HOME="$TOMCAT_HOME-$1"
+    if [ ! -d $TOMCAT_HOME ]; then
+        echo "Environment specified not found: $1"
+        exit
+    fi
+
+    ant war && (
+        $TOMCAT_HOME/bin/shutdown.sh
+        rm -f $TOMCAT_HOME/webapps/gcweb.war
+        rm -fR $TOMCAT_HOME/webapps/gcweb
+        mv gcweb.war $TOMCAT_HOME/webapps/
+        $TOMCAT_HOME/bin/startup.sh
+    )
+
+fi
