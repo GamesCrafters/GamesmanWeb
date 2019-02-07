b361514399d9d5399e6b71f61579ae09f2747936
Zhuo Lu <sethlu@mintkit.net>
2019-02-07 02:19:20 -0800

    Update launch script to support dev environments

--- launch.sh
+++ launch.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
+# Usage: ./launch.sh [env]
+
 TOMCAT_HOME=/var/lib/tomcat8
+
+if [ $# -gt 0 ]; then # environment provided
+    TOMCAT_HOME="$TOMCAT_HOME-$1"
+    if [ ! -d $TOMCAT_HOME ]; then
+        echo "Environment specified not found: $1"
+        exit
+    fi
+else 
+    read -p "Are you sure? (yes/no) " -n 1 -r
+    echo
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+        exit
+    fi
+fi
 
 ant war && (
 systemctl stop tomcat8
